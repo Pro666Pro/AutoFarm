67b9865b4b536f0df63e5cc322ecabc3c7a23c2c
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-29 11:53:28 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -10,14 +10,7 @@ if game.Players.LocalPlayer.leaderstats.Slaps.Value >= 11500 then
 
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Error",Text = "Enough Slaps!",Icon = "rbxassetid://7733658504",Duration = 5})
         
-Setting = { 
-        Time = 18, -- Max is 18 (seconds)
-        Amount = 2000, -- Max is 20000, Maybe 1000 it's the best value for bad devices
-        ErrorReply = "kick", -- (If the script cannot find any server then you will get kicked, You can change to "rejoin" to rejoin if the script can't find any server)
-        SwitchServerType = "serverhop" -- Replace it with "rejoin" if u want to rejoin the current server 
-    }
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Ngigerthebestlolosos/FarmingOhios/refs/heads/main/MyShlongIsLong"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Pro666Pro/PremiumSlapFarm/main/main.lua'))() 
 
 else
 
